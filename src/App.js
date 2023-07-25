4ba7e4bd6710a45c5619ec38d6b2f405e8ec643e
oshaye <oshayewatson@gmail.com>
2023-07-25 04:37:42 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Component } from "react";
 //import { Myheader } from "/src/components/components.js";
-import styles from "/src/styles/styles.css";
-import { About } from "/src/about/about.js";
+
+//import {styles} from "src/styles/styles.css";
+import { About } from "src/about/about.js";
 export default function App() {
   return (
     <div className="App">
